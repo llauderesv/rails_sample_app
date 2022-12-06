79c8defdce8897ebf5773d3e1eb117dcc46b52fa
Vince Llauderes <llauderesv@gmail.com>
2022-12-06 18:47:21 +0800

    remove unused tables

--- db/schema.rb
+++ db/schema.rb
@@ -12,20 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2022_12_06_091007) do
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "products", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "part_number"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
